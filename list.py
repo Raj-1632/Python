3bc16edc459d21a94e6ade3735f12dedf03483ee
Raj-1632 <raj643995@gmail.com>
2025-06-02 10:43:14 +0530

    push

--- list.py
+++ list.py
@@ -1,5 +1,7 @@
-l1 = ['Raj',1632,12.25,'Patel',True]
+l1 = ['Raj',1632,12.25,'Patel',True,1632]
 l2 = ['python',1632,False]
+l3 = [54,78,52,12,34,56,78,90]
+l4 = ['e','r','g','v','j','p']
 print(l1)
 print(l2)
 print(l1[:3])
@@ -10,13 +12,3 @@ l1[1] = 1234
 print(l1)
 del l1[1]
 print(l1)
-
-l1.append(1234)
-print(l1)
-
-l2 = l1.copy() 
-print(l2)
-
-l2.extend(l1)
-print(l2)
-
