96ff3a91bbb21bab0326aea5a88d78f44818544b
Raj-1632 <raj643995@gmail.com>
2025-06-19 14:20:52 +0530

    update

--- new.py
+++ new.py
@@ -1,27 +1,31 @@
 import random as r 
 import string
-def rand_pass(l=8):
-    if l<4:
-        print("Password must be minimum 8 characters.")
 
-   
+def rand_pass(l=8):
+    if l < 4:
+        return "Password length must be at least 4."
+
     uppercase_letters = list(string.ascii_uppercase)
-    
     lowercase_letters = list(string.ascii_lowercase)
-   
     digits = list(string.digits)
-    
     special_characters = list(string.punctuation)
 
-    pw = [ r.choice(uppercase_letters),r.choice(lowercase_letters),r.choice(digits),r.choice(special_characters)]
+    
+    pw = [
+        r.choice(uppercase_letters),
+        r.choice(lowercase_letters),
+        r.choice(digits),
+        r.choice(special_characters)
+    ]
 
-    print(pw)
+    all_ch = uppercase_letters + lowercase_letters + digits + special_characters
 
-    all_ch = uppercase_letters+lowercase_letters+digits+special_characters
-
-    pw = pw+r.choices(all_ch,k= (l-4))
-
+    pw += r.choices(all_ch, k=(l - 4))
     r.shuffle(pw)
 
-    print("\n Password is ",''.join(pw))
+    return ''.join(pw)
+
+length = int(input("Enter desired password length (min 4): "))
+print("Generated Password:", rand_pass(length))
+
 
